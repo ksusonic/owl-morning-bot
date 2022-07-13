ad1afbcd741c7401ac97b2ecbee0ae1d94bfcd1b
Daniil <ksusonic@yandex.ru>
2022-07-14 01:16:41 +0300

    /ping and /pwd handlers

--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -2,17 +2,24 @@ package telegram
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"strconv"
 )
 
 const (
-	commandStart = "start"
+	start = "start"
+	ping  = "ping"
+	pwd   = "pwd"
 )
 
 func (b *Bot) handleCommand(message *tgbotapi.Message) (tgbotapi.Message, error) {
 	msg := tgbotapi.NewMessage(message.Chat.ID, "Я не знаю этого, шайтан")
 	switch message.Command() {
-	case commandStart:
-		msg.Text = "Приветик!"
+	case start:
+		msg.Text = "Здарова! Если есть вопросы, пиши @ksusonic"
+	case ping:
+		msg.Text = "pong 🏓"
+	case pwd:
+		msg.Text = strconv.FormatInt(message.Chat.ID, 10)
 	}
 
 	return b.bot.Send(msg)
