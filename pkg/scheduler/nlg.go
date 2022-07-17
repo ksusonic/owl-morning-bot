05c9bb0431ea09d6271e4b47c26b27f40bb8b731
Daniil <ksusonic@yandex.ru>
2022-07-17 22:09:13 +0300

    Revert "tusovochnyi update"
    This reverts commit 7b8e2ef72e9817c64cb4dbbdce3a427b68d10e69.

--- pkg/scheduler/nlg.go
+++ pkg/scheduler/nlg.go
@@ -13,9 +13,17 @@ func RandomGoodMorningText() string {
 // TODO: get texts on http request
 
 var morningTexts = [...]string{
-	"Знаю, что хорошо потусила вчера 😏\nНу а теперь с добрым утром! 😘",
+	"Самой милой красотке самого доброго утра 😎",
+	"Хәерле иртэ!",
+	"Доброе утро, Алсушка ^^ 💋💋💋",
+	"Кто это у нас еще спит? Или не спит...\nДоброе утро!!!!!",
+	"Опа, Алсундекс, это ты? Доброе утро! 💋🏄",
 }
 
 var morningImages = [...]string{
-	"https://khlopot.net/wp-content/uploads/2019/09/generalnaya-uborka-doma-3.jpg",
+	"https://i.mycdn.me/i?r=AyH4iRPQ2q0otWIFepML2LxRjFo4tAvRWweiiLE_CLPYA",
+	"https://i.pinimg.com/originals/1d/6f/c1/1d6fc18e48aab88f978a30da6592b3d4.jpg",
+	"https://i.pinimg.com/564x/8e/09/31/8e0931c73721302a67b0f521ae936314.jpg",
+	"https://photowords2.ru/pics_max/photowords_ru_8957.jpg",
+	"https://klike.net/uploads/posts/202111/1637049855_3.jpg",
 }
